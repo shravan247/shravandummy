0fbd7fa0353430fd47bda52e5fe19c6835776598
shravan247 <shravansshetty701@gmail.com>
2024-01-24 18:58:59 +0530

    mjb

---
+++ com.domainname.projectname/src/test/java/practiceTestdd/xx.java
@@ -0,0 +1,17 @@
+package practiceTestdd;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class xx{
+public static	WebDriver driver;
+@Test
+public static void youtube() {
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.youtube.com/");
+	driver.findElement(By.name("search_query")).sendKeys("Dj rathan calmdown");
+	driver.findElement(By.xpath("//button[@class='style-scope ytd-searchbox']")).click();
+	
+}}
